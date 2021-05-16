a67a037498e9bc6d647df5ac8b632d683041a50d
cheollie <>
2021-05-16 19:39:23 +0000

    migrated the javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+function sendMessage() {
+  var request = new XMLHttpRequest();
+  request.open("POST", document.getElementById("token").value);
+
+  request.setRequestHeader('Content-type', 'application/json');
+
+  var params = {
+    username: document.getElementById("uname").value,
+    avatar_url: document.getElementById("pfp").value,
+    content: document.getElementById("content").value
+  }
+
+  request.send(JSON.stringify(params));
+}
