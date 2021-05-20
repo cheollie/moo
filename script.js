7cf2c35453e9e803a13a8320d81f657c47d62508
cheollie <>
2021-05-20 03:02:15 +0000

    added a spam mode feature and removed the ability for @everyone or @here pings

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 function sendMessage() {
-  var request = new XMLHttpRequest();
-  request.open("POST", document.getElementById("token").value);
 
+  var request = new XMLHttpRequest();
+  
+  var _token = document.getElementById("token").value;
+  var _username = document.getElementById("uname").value;
+  var _avatarurl = document.getElementById("pfp").value;
+  var _content = document.getElementById("content").value;
+
+  var spamMode = document.getElementById("spamMode");
+
+  request.open("POST", _token);
   request.setRequestHeader('Content-type', 'application/json');
 
-  var params = {
-    username: document.getElementById("uname").value,
-    avatar_url: document.getElementById("pfp").value,
-    content: document.getElementById("content").value
+  if ( _content.includes("@everyone")==true || _content.includes("@here")==true ){
+    _content = _content.replace("@everyone","`@everyone`");
+    _content = _content.replace("@here","`@here`");
   }
 
-  if (document.getElementById("content").value != "") {
+  var params = {
+    username: _username,
+    avatar_url: _avatarurl,
+    content: _content
+  }
+
+  if (_content != "" || _token != "") {
     request.send(JSON.stringify(params));
-    alert("sent (if ur token url is correct at least-)");
+    if (spamMode.checked == false){
+      alert("sent (if ur token url is correct at least-)");
+    }
   }
   else { 
-    alert("message can't be empty");
+    alert("message/token url cannot be empty");
   }
+
+  if (spamMode.checked == false){
+    _content = '';
+  }
+
 }
